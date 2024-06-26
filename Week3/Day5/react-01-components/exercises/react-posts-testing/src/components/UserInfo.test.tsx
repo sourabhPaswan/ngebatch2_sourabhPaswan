4e7b7aae6c5d0ea57483688fdd0cc58c14372703
sourabhpaswan <sourabh.paswan@accenture.com>
2024-06-26 17:11:45 +0000

    React post testing exercise

--- Week3/Day5/react-01-components/exercises/react-posts-testing/src/components/UserInfo.test.tsx
+++ Week3/Day5/react-01-components/exercises/react-posts-testing/src/components/UserInfo.test.tsx
@@ -1,15 +1,41 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-import UserInfo from './UserInfo'
+import UserInfo from "./UserInfo";
 
 /* eslint-enable */
 
-describe('Display UserInfo', () => {
-  it('should render the user\'s name', () => {
+describe("Display UserInfo", () => {
+  it("should render the user's name", () => {
     // todo
-  })
-})
+    render(
+      <UserInfo
+        user={{
+          avatarUrl: "https://robohash.org/Fiona?size=60x60",
+          name: "Fiona Worx",
+        }}
+      />
+    );
+    const foundElement = screen.getByText(/Fiona Worx/);
+    expect(foundElement).toHaveTextContent("Fiona Worx");
+  });
+
+  it("should render the user's image", () => {
+    // todo
+    render(
+      <UserInfo
+        user={{
+          avatarUrl: "https://robohash.org/Fiona?size=60x60",
+          name: "Fiona Worx",
+        }}
+      />
+    );
+    const foundImage = screen.getByAltText(/Fiona Worx avatar/);
+    expect(foundImage).toHaveAttribute(
+      "src",
+      "https://robohash.org/Fiona?size=60x60"
+    );
+  });
+});
